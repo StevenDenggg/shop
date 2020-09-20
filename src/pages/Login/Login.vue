8521c5c0c1a0c4684ce3929ae58d323251d6facb
StevenDenggg <1608689100@qq.com>
2020-09-20 12:37:26 +0800

    update

--- src/pages/Login/Login.vue
+++ src/pages/Login/Login.vue
@@ -2,41 +2,43 @@
     <section class="loginContainer">
         <div class="loginInner">
             <div class="login_header">
-                <h2 class="login_logo">硅谷外卖</h2>
+                <h2 class="login_logo">广外外卖</h2>
                 <div class="login_header_title">
-                    <a href="javascript:;" class="on">短信登录</a>
-                    <a href="javascript:;">密码登录</a>
+                    <a href="javascript:;" :class="{'on':isActive}" @click="isActive=true">短信登录</a>
+                    <a href="javascript:;" :class="{'on':!isActive}" @click="isActive=false">密码登录</a>
                 </div>
             </div>
             <div class="login_content">
                 <form>
-                    <div class="on">
+                    <div :class="{'on':isActive}">
                         <section class="login_message">
-                            <input type="tel" maxlength="11" placeholder="手机号" />
-                            <button disabled="disabled" class="get_verification">获取验证码</button>
+                            <input type="tel" maxlength="11" placeholder="手机号" v-model="phone">
+                            <button :disabled="!/^1\d{10}$/.test(phone)" class="get_verification" 
+                            :class="{right_phone:/^1\d{10}$/.test(phone)}" @click.prevent="getCode">{{leftTime?"已发送"+leftTime+"s":"获取验证码"}}</button>
                         </section>
                         <section class="login_verification">
-                            <input type="tel" maxlength="8" placeholder="验证码" />
+                            <input type="tel" maxlength="8" placeholder="验证码" v-model="code">
                         </section>
                         <section class="login_hint">
-                            温馨提示：未注册硅谷外卖帐号的手机号，登录时将自动注册，且代表已同意
+                            温馨提示：未注册广外外卖帐号的手机号，登录时将自动注册，且代表已同意
                             <a href="javascript:;">《用户服务协议》</a>
                         </section>
                     </div>
-                    <div>
+                    <div :class="{'on':!isActive}">
                         <section>
                             <section class="login_message">
-                                <input type="tel" maxlength="11" placeholder="手机/邮箱/用户名" />
+                                <input type="text" maxlength="11" placeholder="手机/邮箱/用户名" v-model="name">
                             </section>
                             <section class="login_verification">
-                                <input type="tel" maxlength="8" placeholder="密码" />
-                                <div class="switch_button off">
-                                    <div class="switch_circle"></div>
-                                    <span class="switch_text">...</span>
+                                <input type="text" maxlength="8" placeholder="密码" v-if="showPwd" v-model="pwd">
+                                <input type="password" maxlength="8" placeholder="密码" v-else v-model="pwd">
+                                <div class="switch_button" :class="showPwd?'on':'off'" @click="showPwd=!showPwd">
+                                    <div class="switch_circle" :class="{right:showPwd}"></div>
+                                    <span class="switch_text">{{showPwd?"abc":"..."}}</span>
                                 </div>
                             </section>
                             <section class="login_message">
-                                <input type="text" maxlength="11" placeholder="验证码" />
+                                <input type="text" maxlength="11" placeholder="验证码" v-model="captcha">
                                 <img
                                     class="get_verification"
                                     src="./images/captcha.svg"
@@ -61,8 +63,33 @@ export default {
     methods: {
         back() {
             this.$router.back()
+        },
+        getCode(){
+            if(this.leftTime==0){
+                this.leftTime = 10
+                let intervalId = setInterval(()=>{
+                    this.leftTime--
+                    if(this.leftTime<=0){
+                        clearInterval(intervalId)
+                    }
+                },1000)
+            }
+            
         }
     },
+    data() {
+        return {
+            isActive: true,
+            phone:"",
+            leftTime:0,
+            showPwd:false,
+            pwd:"",
+            code:"",
+            name:"",
+            captcha:""
+        }
+    },
+
 };
 </script>
 
@@ -128,6 +155,8 @@ export default {
                             color #ccc
                             font-size 14px
                             background transparent
+                            &.right_phone
+                                color black
                     .login_verification
                         position relative
                         margin-top 16px
@@ -155,7 +184,7 @@ export default {
                                     color #ddd
                             &.on
                                 background #02a774
-                            >.switch_circle
+                            .switch_circle
                                 //transform translateX(27px)
                                 position absolute
                                 top -1px
@@ -167,6 +196,8 @@ export default {
                                 background #fff
                                 box-shadow 0 2px 4px 0 rgba(0,0,0,.1)
                                 transition transform .3s
+                                &.right
+                                    transform translateX(30px)
                     .login_hint
                         margin-top 12px
                         color #999
--- src/pages/Profile/Profile.vue
+++ src/pages/Profile/Profile.vue
@@ -73,7 +73,7 @@
                     <i class="iconfont icon-yooxi"></i>
                 </span>
                 <div class="my_order_div">
-                    <span>硅谷外卖会员卡</span>
+                    <span>广外外卖会员卡</span>
                     <span class="my_order_icon">
                         <i class="iconfont icon-iconfontjiantou3"></i>
                     </span>
